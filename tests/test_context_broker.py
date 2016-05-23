d903be8ed78bceae93ea338ece8a2659860aa773
elruffo <leonardo.ruffin@gmail.com>
2016-05-23 18:53:38 +0200

    Add tests for new features

--- tests/test_context_broker.py
+++ tests/test_context_broker.py
@@ -24,45 +24,73 @@ class PycontextbrokerTestCase(unittest.TestCase):
     def test_get_uptime(self):
         self.assertIsNotNone(self.cbc.get_uptime())
 
-    def test_create_entity(self):
+    def test_create_entity_without_attributes(self):
         response = self.cbc.create_entity(
-            "Search",
-            "search_1",
-            attributes=[{"name": "number", "type": "integer", "value": "0"}]
+            "TestSearch",
+            "test_search_1",
         )
         self.assertIsNotNone(response)
         self.assertIn('contextResponses', response)
+        self.assertNotIn('attributes', response.get('contextResponses')[0])
         self.assertIn('type', response)
-        self.assertEquals(response.get('type'), 'Search')
+        self.assertEquals(response.get('type'), 'TestSearch')
         self.assertIn('id', response)
-        self.assertEquals(response.get('id'), 'search_1')
+        self.assertEquals(response.get('id'), 'test_search_1')
+        self.assertIn('isPattern', response)
+        self.assertEquals(response.get('isPattern'), 'false')
+
+    def test_create_entity_with_attributes(self):
+        response = self.cbc.create_entity(
+            "TestSearch",
+            "test_search_2",
+            attributes=[{"name": "number", "type": "integer", "value": "1"}]
+        )
+        self.assertIsNotNone(response)
+        self.assertIn('contextResponses', response)
+        self.assertIn('attributes', response.get('contextResponses')[0])
+        self.assertIn('type', response)
+        self.assertEquals(response.get('type'), 'TestSearch')
+        self.assertIn('id', response)
+        self.assertEquals(response.get('id'), 'test_search_2')
         self.assertIn('isPattern', response)
         self.assertEquals(response.get('isPattern'), 'false')
 
     def test_get_entity(self):
-        response = self.cbc.get_entity("Search", "search_1")
+        response = self.cbc.get_entity("TestSearch", "test_search_1")
         self.assertIsNotNone(response)
         self.assertIn('contextElement', response)
         self.assertIn('attributes', response.get('contextElement'))
         self.assertEquals('false', response.get('contextElement').get('isPattern'))
-        self.assertEquals('search_1', response.get('contextElement').get('id'))
-        self.assertEquals('Search', response.get('contextElement').get('type'))
+        self.assertEquals('test_search_1', response.get('contextElement').get('id'))
+        self.assertEquals('TestSearch', response.get('contextElement').get('type'))
+        response = self.cbc.get_entity("TestSearch", "test_search_2")
+        self.assertIsNotNone(response)
+        self.assertIn('contextElement', response)
+        self.assertNotIn('attributes', response.get('contextElement'))
+        self.assertEquals('false', response.get('contextElement').get('isPattern'))
+        self.assertEquals('test_search_2', response.get('contextElement').get('id'))
+        self.assertEquals('TestSearch', response.get('contextElement').get('type'))
 
     def test_get_attribute_value(self):
-        if self.cbc.get_entity("Search", "search_1")['statusCode']['code'] == '404':
-            self.cbc.create_entity("Search", "search_1", attributes=[{"name": "number", "type": "integer", "value": "0"}])
-        response = self.cbc.get_attribute_value("Search", "search_1", "number")
+        if self.cbc.get_entity("TestSearch", "test_search_1")['statusCode']['code'] == '404':
+            self.cbc.create_entity("TestSearch", "test_search_1", attributes=[{"name": "number", "type": "integer", "value": "0"}])
+        response = self.cbc.get_attribute_value("TestSearch", "test_search_1", "number")
         self.assertEquals('0', response)
 
     def test_update_attribute_value(self):
-        self.cbc.update_attribute_value("Search", "search_1", "number", 1)
-        self.assertEqual('1', self.cbc.get_attribute_value("Search", "search_1", "number"))
+        self.cbc.update_attribute_value("TestSearch", "test_search_1", "number", 1)
+        self.assertEqual('1', self.cbc.get_attribute_value("TestSearch", "test_search_1", "number"))
 
     def test_delete_entity(self):
-        if self.cbc.get_entity("Search", "search_1")['statusCode']['code'] == '404':
-            self.cbc.create_entity("Search", "search_1", attributes=[{"name": "number", "type": "integer", "value": "0"}])
-        self.cbc.delete_entity("Search", "search_1")
-        self.assertEquals(self.cbc.get_entity("Search", "search_1")['statusCode']['code'], '404')
+        if self.cbc.get_entity("TestSearch", "test_search_1")['statusCode']['code'] == '404':
+            self.cbc.create_entity("TestSearch", "test_search_1", attributes=[{"name": "number", "type": "integer", "value": "0"}])
+        if self.cbc.get_entity("TestSearch", "test_search_2")['statusCode']['code'] == '404':
+            self.cbc.create_entity("TestSearch", "test_search_2",
+                                   attributes=[{"name": "number", "type": "integer", "value": "0"}])
+        self.cbc.delete_entity("TestSearch", "test_search_1")
+        self.assertEquals(self.cbc.get_entity("TestSearch", "test_search_1")['statusCode']['code'], '404')
+        self.cbc.delete_entity("TestSearch", "test_search_2")
+        self.assertEquals(self.cbc.get_entity("TestSearch", "test_search_2")['statusCode']['code'], '404')
 
     def test_subscribe_on_attribute_change(self):
         response = self.cbc.subscribe_on_attribute_change("Search", "search_1", "number", "http://localhost:3030/search_number")
