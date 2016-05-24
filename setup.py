12e793822b64c6788cbda86e1d4f2165d16de0a2
elruffo <leonardo.ruffin@gmail.com>
2016-05-24 17:00:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pycontextbroker',
     packages=['pycontextbroker'],
-    version='0.1.4',
+    version='0.2.0',
     description='An opensource library that makes communications with Orion Context Broker smooth, easy and pythonic',
     author='Leonardo Ruffin',
     author_email='leonardo.ruffin@antlos.com',
     url='https://github.com/Antlos/pycontextbroker',
-    download_url='https://github.com/Antlos/pycontextbroker/tarball/0.1',
+    download_url='https://github.com/Antlos/pycontextbroker/tarball/0.2.0',
     keywords=['orion', 'context', 'broker', 'fiware'],
     classifiers=[],
 )
